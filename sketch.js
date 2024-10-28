0c766e8ad6a96f8270794b115ab13c969e7e5926
Kingnick253 <nicholaswebb2016@gmail.com>
2024-10-27 22:24:40 -0700

    Created setup function to create the canvas for the simulator

--- sketch.js
+++ sketch.js
@@ -14,3 +14,21 @@ function make2DArray(cols, rows){
     }
     return arr;
 }
+
+let grid;
+let w = 10;
+let cols, rows;
+
+function setup() {
+    createCanvas(400,400);
+    cols = width / w;
+    rows = height / w;
+    grid = make2DArray(cols, rows);
+    
+    for(let i = 0; i < cols; i++){
+        for(let j = 0; j < cols; j++){
+            grid[i][j] = 0;
+        }
+    }
+
+    }
